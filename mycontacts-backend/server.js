86bfffc89c1ff0616f56982906c3a24e0e381b09
bkaur-bkj <bhanikaur.juneja.che19@itbhu.ac.in>
2023-07-03 13:16:23 +0530

    basic api

--- mycontacts-backend/server.js
+++ mycontacts-backend/server.js
@@ -4,6 +4,18 @@ const dotenv = require("dotenv").config();
 const app = express();
 
 const port = process.env.PORT || 5000;
+
+//sending response normally as a text
+app.get("api/contacts", (req, res) =>{
+    res.status(200).send("I m good");
+});
+
+//sending response as a json
+app.get("api/contacts", (req, res) =>{
+    res.status(200).json({message: "I am good"});
+});
+
+
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
 });
